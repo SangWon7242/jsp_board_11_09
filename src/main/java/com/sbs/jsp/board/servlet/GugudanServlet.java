ff3aba3bd1062c261c38f876305ab6e768e34407
SangWon7242 <gotkdqja@gmail.com>
2024-11-10 09:51:21 +0900

    구구단jsp에 모델2 방식 적용

--- src/main/java/com/sbs/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/com/sbs/jsp/board/servlet/GugudanServlet.java
@@ -1,6 +1,7 @@
 package com.sbs.jsp.board.servlet;
 
 import com.sbs.jsp.board.Rq;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,11 +19,13 @@ public class GugudanServlet extends HttpServlet {
     int dan = rq.getIntParam("dan", 9);
     int limit = rq.getIntParam("limit", 9);
 
+    req.setAttribute("dan", dan);
+    req.setAttribute("limit", limit);
+    req.setAttribute("number", 10);
 
-    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
-
-    for (int i = 1; i <= limit; i++) {
-      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
-    }
+    // request에 정보를 담는다.
+    // 왜냐하면 나중에 gugudan2.jsp에서 해당 내용을 꺼내 쓸 수 있기 때문이다.
+    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/jsp/gugudan2.jsp");
+    requestDispatcher.forward(req, resp);
   }
 }
---
+++ src/webapp/jsp/gugudan2.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="com.sbs.jsp.board.Rq" %>
+
+<%
+int dan = (int) request.getAttribute("dan");
+int limit = (int) request.getAttribute("limit");
+int number = (int) request.getAttribute("number");
+%>
+
+<h1><%=dan%>단</h1>
+<% for(int i = 1; i <= limit; i++) { %>
+<div><%=dan%> * <%=i%> = <%=dan * i%></div>
+<% } %>
